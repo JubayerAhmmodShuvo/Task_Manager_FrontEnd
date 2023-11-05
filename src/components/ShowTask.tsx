fc677472cdb87c57aa62d4b73b3f8b352222bbf1
Jubayer Ahmmod Shuvo <jubayer15-10807@diu.edu.bd>
2023-11-05 08:41:04 +0600

    search functions added

--- src/components/ShowTask.tsx
+++ src/components/ShowTask.tsx
@@ -18,11 +18,11 @@ export type Task = {
   taskName: string;
   description: string;
   createdAt: string;
-  status: string; 
+  status: string;
 };
 
 const ShowTask = () => {
-    const router = useRouter();
+  const router = useRouter();
   const { data: tasks, isLoading } = useGetAllTasksByUserQuery(
     {},
     {
@@ -36,6 +36,7 @@ const ShowTask = () => {
   const [remove] = useDeleteTaskMutation();
 
   const [statusFilter, setStatusFilter] = useState("all");
+  const [searchTerm, setSearchTerm] = useState(""); 
 
   const onStart = async (taskId: React.Key | null | undefined | string) => {
     try {
@@ -72,36 +73,53 @@ const ShowTask = () => {
 
   const onUpdate = (taskId: React.Key | null | undefined | string) => {
     router.push(`edit/${taskId}`);
-  }
-
+  };
 
   const filterTasksByStatus = (status: string) => {
     return tasks.filter((task: Task) => task.status === status);
   };
 
+
+  const filteredTasks = tasks?.filter((task: Task) =>
+    task.taskName.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div>
-      <div className="btn-group mx-auto my-8 grid grid-cols-2 sm:grid-cols-4 lg:flex lg:flex-row flex-col justify-center gap-2">
-        {["All", "Pending", "Ongoing", "Completed"].map((status) => (
-          <button
-            key={status}
-            className={`${
-              statusFilter.toLowerCase() === status.toLowerCase()
-                ? "bg-blue-500 text-white"
-                : "bg-transparent hover:bg-black text-black hover:text-white"
-            } text-lg py-2 px-4 border border-blue-50 hover:border-transparent rounded font-serif`}
-            onClick={() => setStatusFilter(status.toLowerCase())}
-          >
-            {status}
-          </button>
-        ))}
+      <div className="lg:flex lg:flex-row justify-between items-center ">
+        <div className="md:flex  justify-between items-center my-8">
+          <div className="md:btn-group grid md:grid-cols-2 sm:grid-cols-4 lg:flex lg:flex-row flex-col gap-2">
+            {["All", "Pending", "Ongoing", "Completed"].map((status) => (
+              <button
+                key={status}
+                className={`${
+                  statusFilter.toLowerCase() === status.toLowerCase()
+                    ? "bg-blue-500 text-white"
+                    : "bg-transparent hover:bg-black text-black hover:text-white"
+                } text-lg py-2 px-4 border border-blue-50 hover:border-transparent rounded font-serif`}
+                onClick={() => setStatusFilter(status.toLowerCase())}
+              >
+                {status}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="md:mb-4 mb-4">
+          <input
+            type="text"
+            placeholder="Search by Task Name"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="w-96 py-2 px-3 text-lg rounded-md border border-gray-200"
+          />
+        </div>
       </div>
 
       <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4">
         {isLoading
           ? "Loading..."
           : statusFilter === "all"
-          ? tasks.map((task: Task) => (
+          ? filteredTasks.map((task: Task) => (
               <div key={task._id}>
                 <TaskCard
                   task={task}
